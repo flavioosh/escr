7e830ee0370f1b4901919cfe1834c99804817ced
Szymon Libudzki <flavioosh@gmail.com>
2024-04-05 11:25:00 +0100

    feat: Add base dialog

---
+++ src/components/dialogs/BaseDialog.svelte
@@ -0,0 +1,128 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	export let title: string = '';
+
+	let dialogElement: HTMLDialogElement;
+
+	export function show() {
+		dialogElement.showModal();
+		dispatch('show');
+	}
+
+	export function close() {
+		dialogElement.close();
+		dispatch('close');
+	}
+</script>
+
+<dialog bind:this={dialogElement}>
+	<header>
+		<span>{title}</span>
+		<button on:click={close}>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+				<path
+					d="m249-183-66-66 231-231-231-231 66-66 231 231 231-231 66 66-231 231 231 231-66 66-231-231-231 231Z"
+				/>
+			</svg>
+		</button>
+	</header>
+	<section>
+		<slot />
+	</section>
+	{#if $$slots.footer}
+		<footer>
+			<slot name="footer" />
+		</footer>
+	{/if}
+</dialog>
+
+<style lang="postcss">
+	dialog {
+		border: none;
+		padding: 0 0 1.5rem;
+
+		min-width: 30vw;
+		max-height: 80vh;
+
+		/* Can't apply flex directly to dialog as it starts with display: none */
+		&[open] {
+			display: flex;
+		}
+		flex-direction: column;
+
+		overflow: hidden;
+
+		box-shadow: 0 0.125rem 0.375rem 0 rgba(0, 0, 0, 0.2);
+
+		header {
+			height: 3rem;
+
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+
+			background-color: var(--color-pink);
+			color: #fff;
+
+			span {
+				padding: 0 1rem;
+
+				font-size: 1.25rem;
+				font-weight: 500;
+			}
+
+			button {
+				border: none;
+				padding: 0.25rem;
+
+				line-height: 0;
+
+				background: none;
+
+				cursor: pointer;
+
+				transition: box-shadow 0.2s;
+
+				&:focus-visible {
+					outline: none;
+
+					box-shadow: inset 0 0 0 0.25rem var(--color-yellow);
+				}
+
+				svg {
+					width: 2.5rem;
+					height: 2.5rem;
+
+					fill: #fff;
+				}
+			}
+		}
+
+		section {
+			padding: 1.5rem 1.5rem 0;
+
+			display: flex;
+			flex-direction: column;
+			gap: 2rem;
+
+			overflow-y: auto;
+		}
+
+		footer {
+			padding: 1.5rem 1.5rem 0;
+		}
+
+		&::backdrop {
+			background-color: rgba(0, 0, 0, 0.4);
+		}
+	}
+
+	@media (max-width: 1024px) {
+		dialog {
+			width: 85vw;
+		}
+	}
+</style>
---
+++ src/components/dialogs/DialogActions.svelte
@@ -0,0 +1,30 @@
+<script context="module" lang="ts">
+	import type { ButtonColor } from '$components/Button.svelte';
+
+	export type DialogAction = {
+		label: string;
+		color: ButtonColor;
+		fn: () => void;
+	};
+</script>
+
+<script lang="ts">
+	import Button from '$components/Button.svelte';
+
+	export let actions: DialogAction[];
+</script>
+
+<div>
+	{#each actions as { label, color, fn }}
+		<Button {color} on:click={fn}>
+			{label}
+		</Button>
+	{/each}
+</div>
+
+<style lang="postcss">
+	div {
+		display: flex;
+		justify-content: center;
+	}
+</style>
